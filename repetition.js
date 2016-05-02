738bbe14dbb34d6b52003ada371ba7f11e23c7db
Rilag001 <rickylagerkvist@hotmail.com>
2016-05-02 13:26:02 +0200

    Updated repetition.js

--- repetition.js
+++ repetition.js
@@ -1,6 +1,7 @@
 // Array
 
-// Medtod 1, initierar  
+
+// Medtod 1
 var weekdays = ["Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag", "Söndag"];
 
 //Hämta ut array
@@ -9,3 +10,20 @@ console.log("Första veckodagen " + weekdays[0]);
 var favoritDay = weekdays[4];
 
 // Medtod 2
+var months [];
+months[0] = "Januari";
+months[1] = "Februari";
+months[2] = "Mars";
+months[3] = "April";
+months[4] = "Maj";
+months[5] = "Juni";
+months[6] = "Juli";
+months[7] = "Augusti";
+months[8] = "September";
+months[9] = "Oktober";
+months[10] = "November";
+months[11] = "December";
+
+console.log("Året har " + month.length + " månader.");
+
+months.push("Narniaari");
