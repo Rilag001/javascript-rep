3ec6226e141d7c626ec842d7db3ebfc435ecae2b
Rilag001 <rickylagerkvist@hotmail.com>
2016-05-02 13:15:24 +0200

    Created repetition.js

--- repetition.js
+++ repetition.js
@@ -1,1 +1,11 @@
-// javascript
+// Array
+
+// Medtod 1, initierar  
+var weekdays = ["Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag", "Söndag"];
+
+//Hämta ut array
+console.log("Första veckodagen " + weekday[0]);
+
+var favoritday = weekday[4];
+
+// Medtod 2
